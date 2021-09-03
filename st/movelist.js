72b7b8336d08a1ac120d95c75b0592b00d0ddb91
yann-lcrx <yannick84v@gmail.com>
2021-09-03 17:38:28 +0200

    Corrected dirname typo

--- st/movelist.js
+++ st/movelist.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
+import { readdirSync } from 'fs';
 
-const moveList = fs.readdirSync('__dirname');
+const moveList = readdirSync(__dirname);
 console.log(moveList);
 moveList.forEach(move => {
   console.log(move);
@@ -18,14 +18,8 @@ moveList.forEach(move => {
       <td colspan="2">
         <table>
           <tr>
-            <td>
-              <div style='padding-top:3px'><img id=
-              'crouching1crjpg' class='movesetimg hoverbox' style=
-              'float:left' src='./crouching/1_cr.jpg'><img id=
-              'crouching2crjpg' class='movesetimg hoverbox' style=
-              'float:left' src='./crouching/2_cr.jpg'><img id=
-              'crouching3crjpg' class='movesetimg hoverbox' style=
-              'float:left' src='./crouching/3_cr.jpg'></div>
+            <td id="attack-parts">
+              
             </td>
           </tr>
         </table>
