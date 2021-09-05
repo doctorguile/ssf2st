717f7322c8a88b8635a08925c052798b84fc51fe
yann-lcrx <yannick84v@gmail.com>
2021-09-05 13:41:51 +0200

    New chars visuals, edited generation scripts

--- st/blanka/build_index.sh
+++ st/blanka/build_index.sh
@@ -6,35 +6,21 @@ sed -n '/<\/section id="hitbox-list">/, $p' index.html > bottom.html
 echo "" > table.html
 
 for move in $(echo */ | tr -d "/"); do
-    echo "  <a name=\"$move\" id=\"$move\"></a>" >> table.html
-    echo '  <table cellspacing="0" cellpadding="0" width="100%">' >> table.html
-    echo '    <tr class="movename">' >> table.html
-    echo "      <td class=\"movename\">$move</td>" >> table.html
-    echo '      <td class="back-to-top" align="right">' >> table.html
-    echo '      </td>' >> table.html
-    echo '    </tr>' >> table.html
-    echo '    <tr>' >> table.html
-    echo '      <td colspan="2">' >> table.html
-    echo '        <table>' >> table.html
-    echo '          <tr>' >> table.html
-    echo '            <td>' >> table.html
-    echo '              <div style="padding-top:3px">' >> table.html
+    echo "<div id=\"$move\" class=\"move\">
+      <div class=\"heading\">
+        <h2>$move</h2>
+        <a href=\"\#top\">Back to top</a>
+      </div>
+      <div class=\"screenshots\">" >> table.html
 
     for image in $(find $move -name "*.png" | sort -V); do
 	image_id=$(basename $image | cut -d "." -f 1)
-	echo "		<img id=\"$image_id\" class=\"movesetimg hoverbox\"" >> table.html
-	echo "		     style=\"float:left\" src=\"./$image\">" >> table.html
+	echo "<img id=\"$image_id\" alt=\"$move\" class=\"movesetimg hoverbox\" src=\"./$image\">" >> table.html
     done
     
-    echo '            </td>' >> table.html
-    echo '          </tr>' >> table.html
-    echo '        </table>' >> table.html
-    echo '      </td>' >> table.html
-    echo '    </tr>' >> table.html
-    echo '  </table>' >> table.html
-    echo '  <br>' >> table.html
+    echo "</div>
+    </div>" >> table.html
 done
 
 cat top.html table.html bottom.html > index.html
 rm top.html bottom.html table.html
-
