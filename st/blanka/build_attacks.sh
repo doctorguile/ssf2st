ba7cd37d767b2b938e65c498ad3b4652a29bbc7c
Antoine <Toufadev>
2021-09-05 14:52:35 +0200

    correction build_attacks

--- st/blanka/build_attacks.sh
+++ st/blanka/build_attacks.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-sed '/</ul id="attack-index">/q' index.html > top.html
+sed '/<ul id="attack-index">/q' index.html > top.html
 sed -n '/<\/ul id="attack-index">/, $p' index.html > bottom.html
 
 echo "" > attacks.html
 
 for move in $(echo */ | tr -d "/"); do
-    echo "<li><a href='#$move'>$move</a></li>" >> attacks.html
-
-    for image in $(find $move -name "*.png" | sort -V); do
-	image_id=$(basename $image | cut -d "." -f 1)
-	echo "<img id=\"$image_id\" alt=\"$move\" class=\"movesetimg hoverbox\" src=\"./$image\">" >> attacks.html
-    done
-    
-    echo "</div>
-    </div>" >> attacks.html
+    echo "<li><a href='#"$move"'>$move</a></li>" >> attacks.html
 done
 
 cat top.html attacks.html bottom.html > index.html
--- st/blanka/build_index.sh
+++ st/blanka/build_index.sh
@@ -6,12 +6,13 @@ sed -n '/<\/section id="hitbox-list">/, $p' index.html > bottom.html
 echo "" > table.html
 
 for move in $(echo */ | tr -d "/"); do
-    echo "<div id=\"$move\" class=\"move\">
-      <div class=\"heading\">
-        <h2>$move</h2>
-        <a href=\"\#top\">Back to top</a>
-      </div>
-      <div class=\"screenshots\">" >> table.html
+    echo "
+     <div id=\"$move\" class=\"move\">
+       <div class=\"heading\">
+         <h2>$move</h2>
+         <a href=\"#top\">Back to top</a>
+       </div>
+     <div class=\"screenshots\">" >> table.html
 
     for image in $(find $move -name "*.png" | sort -V); do
 	image_id=$(basename $image | cut -d "." -f 1)
