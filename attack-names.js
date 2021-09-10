9c1ed5ea7b7072edbbd74baa7210f769359ecdb3
yann-lcrx <yannick84v@gmail.com>
2021-09-10 17:52:36 +0200

    Expanded rename script, blanka/boxer gifs + moves

--- attack-names.js
+++ attack-names.js
@@ -3,8 +3,10 @@ const attackMoves = document.querySelectorAll('.heading h2');
 
 document.addEventListener('DOMContentLoaded', function() {
         for (let attack of attackIndexes) {
-            let newValue = attack.innerText
-            .replace(/_/g, ' ')
+            if (attack.innerText.includes("stand_close") == true || attack.innerText.includes("stand_far") == true) {
+                attack.innerText = attack.innerText.replace('stand_', '')
+            }
+            let newValue = attack.innerText.replace(/_/g, ' ')
             attack.innerText = newValue.charAt(0).toUpperCase() + newValue.slice(1)
         };
         for (let attack of attackMoves) {
