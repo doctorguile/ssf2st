ba03a6d47d6056a26ba0ae00e8de2c13827dd434
yann-lcrx <yannick84v@gmail.com>
2021-09-21 22:47:33 +0200

    Zoom in/out OK

--- imageEdit.js
+++ imageEdit.js
@@ -1,6 +1,13 @@
 document
     .getElementById('zoomin')
     .addEventListener('click', function() {
+        if (document.getElementById('canvas-zone').classList.contains('zoomedIn')) {
+            document
+                .getElementById('canvas-zone')
+                .classList.remove('zoomedIn')
+                return ''
+        }
+        //console.log(document.getElementById('canvas-zone').classList.contains('zoomedIn'));
         document
             .getElementById('canvas-zone')
             .classList.add('zoomedIn')
